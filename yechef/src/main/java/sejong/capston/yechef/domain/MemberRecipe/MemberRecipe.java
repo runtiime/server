2a1862216bba5ea31999c6d3564e33867165d2f7
username056 <didtnsdl0406@gmail.com>
2025-04-16 21:06:39 +0900

    [feat] Member-Recipe 중간 테이블 엔티티 구현

---
+++ yechef/src/main/java/sejong/capston/yechef/domain/MemberRecipe/MemberRecipe.java
@@ -0,0 +1,48 @@
+package sejong.capston.yechef.domain.MemberRecipe;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import sejong.capston.yechef.domain.Member.Member;
+import sejong.capston.yechef.domain.Recipe.Recipe;
+import sejong.capston.yechef.global.entity.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE member_recipe SET is_deleted = true, deleted_at = now() WHERE id = ?")
+@SQLRestriction("is_deleted IS FALSE")
+public class MemberRecipe extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long Id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "recipe_id", nullable = false)
+  private Recipe recipe;
+
+  @NonNull
+  private Boolean isLiked = false;
+
+  @NonNull
+  @Column(nullable = false)
+  private LocalDateTime savedAt;
+
+}
