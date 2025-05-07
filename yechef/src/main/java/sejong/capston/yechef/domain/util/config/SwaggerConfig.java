2f059db35875e4a7c98459e2bcbd7a8577183138
username056 <didtnsdl0406@gmail.com>
2025-05-07 17:46:11 +0900

    [feat] 보안(SecurityConfig), SwaggerConfig 및 application.yml 설정 추가

---
+++ yechef/src/main/java/sejong/capston/yechef/domain/util/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package sejong.capston.yechef.domain.util.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("My API Docs")
+            .version("1.0")
+            .description("API 문서 설명"))
+        .servers(List.of(
+            new Server().url("http://localhost:8080").description("Local 개발 서버"),
+            new Server().url("https://43.203.72.240:8080").description("운영 서버")
+        ));
+
+  }
+}
+
+
+
+
+
