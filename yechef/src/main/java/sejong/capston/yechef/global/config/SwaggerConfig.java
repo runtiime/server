78c59612e20915697bd059f9af5e93356406bb0a
username056 <didtnsdl0406@gmail.com>
2025-05-17 21:52:11 +0900

    [fix] main server url 수정

--- yechef/src/main/java/sejong/capston/yechef/global/config/SwaggerConfig.java
+++ yechef/src/main/java/sejong/capston/yechef/global/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
             .description("API 문서 설명"))
         .servers(List.of(
             new Server().url("http://localhost:8080").description("Local 개발 서버"),
-            new Server().url("https://43.203.72.240:8080").description("운영 서버")
+            new Server().url("http://43.203.72.240:8080").description("운영 서버")
         ));
 
   }
