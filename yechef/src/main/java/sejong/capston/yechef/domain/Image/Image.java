e6cfc75600bf17c0fd125afaec61301d24190483
username056 <didtnsdl0406@gmail.com>
2025-05-20 16:03:23 +0900

    [feat] 업로드 키 지정 및 삭제 기능 추가

--- yechef/src/main/java/sejong/capston/yechef/domain/Image/Image.java
+++ yechef/src/main/java/sejong/capston/yechef/domain/Image/Image.java
@@ -11,11 +11,18 @@ import sejong.capston.yechef.domain.Recipe.Recipe;
 @Builder
 public class Image {
 
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String s3Url;
+  private String s3Key; // ← 키는 동적으로 외부에서 생성해서 주입해야 함
 
   @OneToOne(fetch = FetchType.LAZY)
   private Recipe recipe;
+
+  public Image(String s3Url, String s3Key) {
+    this.s3Url = s3Url;
+    this.s3Key = s3Key;
+  }
 }
--- yechef/src/main/java/sejong/capston/yechef/domain/Image/service/ImageService.java
+++ yechef/src/main/java/sejong/capston/yechef/domain/Image/service/ImageService.java
@@ -20,19 +20,30 @@ public class ImageService {
   private final KakaoImageService kakaoImageService;
 
   @Transactional
-  public void generateAndSaveImageForRecipe(Long recipeId) {
+  public void generateAndSaveThumbnail(Long recipeId) {
     Recipe recipe = recipeRepository.findById(recipeId)
-        .orElseThrow(() -> BaseException.from(ErrorCode.RECIPE_NOT_EXIST, "해당 레시피가 존재하지 않습니다."));
+        .orElseThrow(() -> BaseException.from(ErrorCode.RECIPE_NOT_EXIST));
 
     String imageUrl = kakaoImageService.getTopImageUrl(recipe.getTitle());
+    String s3Key = extractKeyFromUrl(imageUrl); // 외부 URL이면 null
 
-    Image image = Image.builder()
+    Image thumbnailImage = Image.builder()
         .s3Url(imageUrl)
+        .s3Key(s3Key)
         .recipe(recipe)
         .build();
 
-    imageRepository.save(image);
-
-    recipe.setImage(image);
+    imageRepository.save(thumbnailImage);
+    recipe.setThumbnailImage(thumbnailImage);
   }
+
+
+  private String extractKeyFromUrl(String url) {
+    // Kakao URL이면 key가 없는 외부 이미지일 수도 있음
+    if (url == null || !url.contains(".amazonaws.com/")) return null;
+
+    // ex: https://bucket-name.s3.region.amazonaws.com/source-images/abc.jpg
+    return url.substring(url.indexOf(".com/") + 5);
+  }
+
 }
