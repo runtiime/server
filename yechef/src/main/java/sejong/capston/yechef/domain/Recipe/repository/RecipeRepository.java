1cd71413fec188f9ec45a36673eccceb475b6741
username056 <didtnsdl0406@gmail.com>
2025-05-20 12:48:01 +0900

    [feat] 공개 레시피 조회 기능 추가

--- yechef/src/main/java/sejong/capston/yechef/domain/Recipe/repository/RecipeRepository.java
+++ yechef/src/main/java/sejong/capston/yechef/domain/Recipe/repository/RecipeRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import sejong.capston.yechef.domain.Recipe.Recipe;
+import sejong.capston.yechef.domain.Recipe.Recipe.RecipeType;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
   @Query("SELECT r FROM Recipe r JOIN r.memberRecipes mr WHERE mr.member.id = :memberId")
   List<Recipe> findByMemberId(@Param("memberId") Long memberId);
 
+  List<Recipe> findByRecipeType(RecipeType recipeType);
 }
