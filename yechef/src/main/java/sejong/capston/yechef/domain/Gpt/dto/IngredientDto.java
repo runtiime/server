24f5dc8b16008aa8059e8ae46f7efafaf9d5dd86
iamseojin <jsj05300@naver.com>
2025-05-21 06:09:07 +0900

    [feat] 레시피 상세 조회 기능 재료 및 용량 조회 추가

--- yechef/src/main/java/sejong/capston/yechef/domain/Gpt/dto/IngredientDto.java
+++ yechef/src/main/java/sejong/capston/yechef/domain/Gpt/dto/IngredientDto.java
@@ -1,15 +1,23 @@
 package sejong.capston.yechef.domain.Gpt.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import sejong.capston.yechef.domain.Ingredient.Ingredient;
+import sejong.capston.yechef.domain.Recipe.dto.RecipeStepDto;
+import sejong.capston.yechef.domain.RecipeSteps.RecipeStep;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class IngredientDto {
     private String name;
     private String quantity;
+
+    public static IngredientDto from(Ingredient ingredient) {
+        return IngredientDto.builder()
+                .name(ingredient.getOriginalName())
+                .quantity(ingredient.getOriginalAmount())
+                .build();
+    }
 }
