7876b299ce87a27bebc7182a18714a26a7f42269
username056 <didtnsdl0406@gmail.com>
2025-05-21 16:07:56 +0900

    [feat] OcrClient에 imageUrl 기반 OCR 요청 로직 구현

--- yechef/src/main/java/sejong/capston/yechef/domain/Recipe/ai/OcrClient.java
+++ yechef/src/main/java/sejong/capston/yechef/domain/Recipe/ai/OcrClient.java
@@ -1,4 +1,31 @@
 package sejong.capston.yechef.domain.Recipe.ai;
 
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import sejong.capston.yechef.domain.Gpt.dto.RecipeParseResultDto;
+
+@Component
 public class OcrClient {
+
+  @Qualifier("aiWebClient")
+  private final WebClient webClient;
+
+  public OcrClient(@Qualifier("aiWebClient") WebClient webClient) {
+    this.webClient = webClient;
+  }
+
+
+  public RecipeParseResultDto extractText(String imageUrl) {
+    return webClient.post()
+        .uri("/ocr/parse")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(Map.of("imageUrl", imageUrl))
+        .retrieve()
+        .bodyToMono(RecipeParseResultDto.class)
+        .block();
+  }
+
 }
